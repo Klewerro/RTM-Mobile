160bbeec4ade5d956e491698c335458ea785c52a
PSzymon <pol.szym95@gmail.com>
2019-08-24 01:28:55 +0100

    Displaying original busStop name on listViews and details page.
    Picking correct ListView item template using DataTemplateSelector. Templates added in App.xaml and NearbyPage.
    Displaying original busStop name if changed (converter for detecting empty string used).

---
+++ Rtm/Rtm/Helpers/BusStopDataTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using Rtm.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Rtm.Helpers
+{
+    public class BusStopDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NameTemplate { get; set; }
+        public DataTemplate CustomNameTemplate { get; set; }
+
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var busStop = item as BusStop;
+            return string.IsNullOrEmpty(busStop.CustomName) ? NameTemplate : CustomNameTemplate;
+        }
+    }
+}
