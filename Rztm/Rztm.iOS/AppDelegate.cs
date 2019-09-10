12875193006ba02aab7a17d7245851973335236a
PSzymon <pol.szym95@gmail.com>
2019-09-10 18:53:30 +0100

    iOS project created.
    Added necessary packages.
    Added necessary permissions.

---
+++ Rztm/Rztm.iOS/AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Rztm.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            //// create a new window instance based on the screen size
+            //Window = new UIWindow(UIScreen.MainScreen.Bounds);
+            //Window.RootViewController = new UIViewController();
+
+            //// make the window visible
+            //Window.MakeKeyAndVisible();
+
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+            return base.FinishedLaunching(application, launchOptions);
+        }
+
+    }
+}
+
+
