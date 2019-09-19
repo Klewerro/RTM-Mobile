8e2fd951d3eb7a00666716909b881bef8e25f4f4
PSzymon <pol.szym95@gmail.com>
2019-09-19 17:19:37 +0100

    Displaying AddToFavorites ToolbarItem on iOS correctly.
    Only on iOS mentioned primary toolbar item must be moved to bottom (as
    secondary), cause of only single line titles are allowed on iOS [without
    custom renderers], and primary icon colors wont change after click [again,
    without custom renderer]

--- Rztm/Rztm/CustomControls/ToggleableToolbarItem.cs
+++ Rztm/Rztm/CustomControls/ToggleableToolbarItem.cs
@@ -10,14 +10,20 @@ namespace Rztm.CustomControls
     {
         public bool IsToggled
         {
-            get => (bool)GetValue(IsToggledProperty); 
-            set => SetValue(IsToggledProperty, value); 
+            get => (bool)GetValue(IsToggledProperty);
+            set => SetValue(IsToggledProperty, value);
         }
 
         public ImageSource IconImageSourceToggled
         {
             get => (ImageSource)GetValue(IconImageSourceToggledProperty);
             set => SetValue(IconImageSourceToggledProperty, value);
+        }
+
+        public String TextToggled
+        {
+            get => (string)GetValue(TextToggledProperty);
+            set => SetValue(TextToggledProperty, value);
         }
 
 
@@ -33,26 +39,54 @@ namespace Rztm.CustomControls
                 typeof(ImageSource),
                 typeof(ToggleableToolbarItem));
 
+        public static readonly BindableProperty TextToggledProperty
+            = BindableProperty.Create(nameof(TextToggled),
+                typeof(string),
+                typeof(ToggleableToolbarItem));
+
+       
+        protected override void OnParentSet()
+        {
+            this.Order = Device.RuntimePlatform == Device.Android
+                ? ToolbarItemOrder.Primary : ToolbarItemOrder.Secondary;
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                this.IconImageSource = null;
+                this.IconImageSourceToggled = null;
+            }
+        }
+
         private static void OnIsVisibleChanged(BindableObject bindable, object oldValue, object newValue)
         {
             var item = bindable as ToggleableToolbarItem;
 
             var newValueBool = (bool)newValue;
             var oldValueBool = (bool)oldValue;
-            
-            if (!newValueBool && oldValueBool)  //!newValueBool && oldValueBool
+
+            if (!newValueBool && oldValueBool)
+            {
+                ToogleProperties(item);
+            }
+
+            if (newValueBool && !oldValueBool)
+            {
+                ToogleProperties(item);
+            }
+        }
+
+        private static void ToogleProperties(ToggleableToolbarItem item)
+        {
+            if (Device.RuntimePlatform == Device.Android)
             {
                 var tempIS = item.IconImageSource;
                 item.IconImageSource = item.IconImageSourceToggled;
                 item.IconImageSourceToggled = tempIS;
             }
 
-            if (newValueBool && !oldValueBool)  //newValueBool && !oldValueBool
-            {
-                var tempIS = item.IconImageSource;
-                item.IconImageSource = item.IconImageSourceToggled;
-                item.IconImageSourceToggled = tempIS;
-            }
+            var tempText = item.Text;
+            item.Text = item.TextToggled;
+            item.TextToggled = tempText;
         }
     }
 }
