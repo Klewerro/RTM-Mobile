816c7713b25ab22228f551e896bc35ab465dae20
PSzymon <pol.szym95@gmail.com>
2019-09-19 17:28:36 +0100

    Enhancement: Different busStop departure time color, when it is in minutes

---
+++ Rztm/Rztm/Converters/IsTimeInMinutesConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Rztm.Converters
+{
+    public class IsTimeInMinutesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var distanceText = (string)value;
+            if (string.IsNullOrEmpty(distanceText))
+                return false;
+
+            return distanceText.Contains("min") ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Conver back is not implemented yet!");
+        }
+    }
+}
