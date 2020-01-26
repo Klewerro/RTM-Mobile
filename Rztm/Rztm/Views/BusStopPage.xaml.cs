f9b51776cc3227756146d9d11020b04ca12b3ba6
PSzymon <pol.szym95@gmail.com>
2020-01-26 18:20:46 +0100

    Enhancement: <1min BusStop time Label blinking

--- Rztm/Rztm/Views/BusStopPage.xaml.cs
+++ Rztm/Rztm/Views/BusStopPage.xaml.cs
@@ -12,9 +12,14 @@ namespace Rztm.Views
     [DesignTimeVisible(false)] 
     public partial class BusStopPage : ContentPage
     {
+        private int _counter = 0;
+        private List<Label> _labelsToBlink;
+        private bool _isBlinking;
+
         public BusStopPage()
         {
             InitializeComponent();
+            _labelsToBlink = new List<Label>(10);
         }
 
         private void RenameToolbarItem_Clicked(object sender, EventArgs e)
@@ -44,5 +49,49 @@ namespace Rztm.Views
                 await ArrowButton.RotateTo(0, easing: Easing.SinInOut);
             }
         }
+
+        private async void TimeLabel_BindingContextChanged(object sender, EventArgs e)
+        {
+            _counter++;
+            var label = sender as Label;
+
+            if (_counter % 11 == 0)
+            {
+                _labelsToBlink.Clear();
+            }
+
+            if (label.Text == null)
+            {
+                _counter = 11;
+                return;
+            }
+
+
+            if (label.Text != null && label.Text.Contains("<"))
+            {
+                _labelsToBlink.Add(label);
+            }
+
+            //10 means last time label in view checked (first iteration) -> start task
+            if (_counter >= 10 && !_isBlinking && _labelsToBlink.Count > 0)
+            {
+                _isBlinking = true;
+                await BlinkLabels(_labelsToBlink, _labelsToBlink[0].TextColor, 1000);
+            }
+        }
+
+
+        private async Task BlinkLabels(List<Label> labels, Color initialColor, int delayTime)
+        {
+            while (true)
+            {
+                await Task.Delay(delayTime);
+                for (int i = 0; i < labels.Count; i++)
+                {
+                    labels[i].TextColor = labels[i].TextColor == initialColor ? Color.Transparent : initialColor;
+                }
+            }
+        }
+
     }
 }
